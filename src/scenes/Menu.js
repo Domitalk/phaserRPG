5f4f259eb42508e1fece0f441a438c3fc6cd8881
Dominic Chu <dominic.m.chu@gmail.com>
2020-11-15 17:18:36 -0500

    give up on functional programming

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -1,7 +1,9 @@
 import Phaser from '../lib/phaser.js';
 
+
 export default class Menu extends Phaser.Scene {
     constructor() {
+        // remember the old school way, constructor is like useEffect(() => {},[]) aka componentDidMount
         super('Menu')
     }
     preload() {

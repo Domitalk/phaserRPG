4f355dec92123ef2d190a2d1a37c4b88df16e1e6
Dominic Chu <dominic.m.chu@gmail.com>
2020-11-15 17:29:19 -0500

    added background for menu

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -4,11 +4,11 @@ import Phaser from '../lib/phaser.js';
 export default class Menu extends Phaser.Scene {
     // try to remember, constructor is like vanilla js version of componentDidMount/useEffect(() => {}, [])
     constructor() {
-        // every Phaser.Scene has to define a unique key, so this is like calling Phaser.Scene('Menu')
+        // every Phaser.Scene has to define a unique key, so this is like calling Phaser.Scene('Menu') or Phaser.Scene({ key: 'Menu' })
         super('Menu')
     }
     preload() {
-        this.load.image('', '')
+        this.load.image('background', 'assets/backgrounds/MenuBackground.png')
     }
     create() {
 

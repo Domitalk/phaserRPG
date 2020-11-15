662115045dce30712e4d26c34b3f636fe1a95def
Dominic Chu <dominic.m.chu@gmail.com>
2020-11-15 16:59:05 -0500

    designate Menu as a scene

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Phaser from './lib/phaser.js';
+import Menu from './scenes/Menu.js';
 
 let config = {
-    type: Phaser.AUTO
+    type: Phaser.AUTO, 
+    scene: Menu
 };
 
 export default new Phaser.Game(config);
---
+++ src/scenes/Menu.js
@@ -0,0 +1,13 @@
+import Phaser from '../lib/phaser.js';
+
+export default class Menu extends Phaser.Scene {
+    constructor() {
+        super('Menu')
+    }
+    preload() {
+        this.load.image('', '')
+    }
+    create() {
+
+    }
+}
